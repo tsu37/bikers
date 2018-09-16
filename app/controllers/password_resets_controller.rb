c9cb898716b19b2a687565447fa755ebd5e6ccd1
Tsutomu Kurafuji <bull.bull.bull91@gmail.com>
2018-09-16 05:47:10 +0000

    見出し変更、英語表記残り対応など

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,10 +11,10 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = "パスワードリセットのメールをお送りしました。"
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = "メールアドレスが見つかりません。"
       render 'new'
     end
   end
@@ -28,7 +28,7 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update_attributes(user_params)          # (4) への対応
       log_in @user
-      flash[:success] = "Password has been reset."
+      flash[:success] = "パスワードをリセットしてください。"
       redirect_to @user
     else
       render 'edit'                                     # (2) への対応
@@ -58,7 +58,7 @@ class PasswordResetsController < ApplicationController
     # トークンが期限切れかどうか確認する
     def check_expiration
       if @user.password_reset_expired?
-        flash[:danger] = "Password reset has expired."
+        flash[:danger] = "パスワードリセットが完了しました。"
         redirect_to new_password_reset_url
       end
     end
